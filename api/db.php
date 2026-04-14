99cb7a2c4b9911dc639f22d8faa79fc704c53d2f
rayan945 <rayansainsard88@gmail.com>
2026-04-15 00:17:03 +0200

    update db credentials

--- api/db.php
+++ api/db.php
@@ -4,9 +4,9 @@ function getPDO(): PDO {
     if ($pdo === null) {
         try {
             $pdo = new PDO(
-                'mysql:host=sql7.freesqldatabase.com;dbname=sql7821663;charset=utf8',
-                'sql7821663',
-                'eH2vyjHlEa'
+                'mysql:host=sql7.freesqldatabase.com;dbname=sql7823249;charset=utf8',
+                'sql7823249',
+                'PCAG4XI7E4'
             );
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
